3a9736df580a6e35892c5c70a3d72574606d32e3
Dan Mohl <dmohl@yahoo.com>
2011-11-22 09:23:46 -0600

    Added functionality to choose the correct FSharp.Core lib based on the TargetFrameworkVersion.

--- src/View/obj/x86/Release/MainWindow.g.i.cs
+++ src/View/obj/x86/Release/MainWindow.g.i.cs
@@ -9,6 +9,8 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
+using FSharpWpfMvvmTemplate.Behavior;
+using FSharpWpfMvvmTemplate.ViewModel;
 using System;
 using System.Diagnostics;
 using System.Windows;
